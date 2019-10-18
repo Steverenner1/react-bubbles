1823bad20fa77878ec133a407ba0d61f33180323
Steve Renner <che1111@hotmail.com>
2019-10-18 08:46:27 -0700

    Added Login and PrivateRoute

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,51 @@
 import React from "react";
+import { useState } from "react";
+import axiosWithAuth from "../utils/AxiosWithAuth";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = props => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+
+  const onChangeHandler = e => {
+    return setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  };
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("http://localhost:5000/api/login", credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("bubbles");
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+    <div>
+      <h1>Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={onChangeHandler}
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="Password"
+          onChange={onChangeHandler}
+        />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  )
 };
 
 export default Login;
